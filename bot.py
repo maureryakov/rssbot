d31ee01f77ef364960c59939101e24e948b5d363
Yakov Maurer <73276809+maureryakov@users.noreply.github.com>
2023-05-28 12:11:36 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,4 @@
 import logging
-from turtle import title
-
 import feedgen.feed
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
@@ -10,40 +8,40 @@ from aiogram.utils.markdown import text
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 # инициализируем бота и диспетчер
-bot = Bot(token='2102525803:AAEaZO3kudDFjcqvmUH9E2GamY354XPM40k')
+bot = Bot(token="2102525803:AAEaZO3kudDFjcqvmUH9E2GamY35*********")
 dp = Dispatcher(bot)
 
 # обработчик команды /start
-@dp.message_handler(commands=['start'])
+@dp.message_handler(commands=["start"])
 async def start(message: types.Message):
     # отправляем приветственное сообщение
-    await message.answer('Привет! Отправьте мне ссылку, и я сделаю из нее RSS-канал.')
+    await message.answer("Привет! Отправьте мне ссылку, и я сделаю из нее RSS-канал.")
 
 # обработчик сообщений с ссылками
-@dp.message_handler(regexp=r'^http.*')
+@dp.message_handler(regexp=r'^https?://')
 async def get_link(message: types.Message):
     link = message.text
     # здесь можно сохранить ссылку в базу данных или в какой-то файл
-    await message.answer('Отлично! Теперь отправьте команду /generate, чтобы создать RSS-канал.')
+    await message.answer("Отлично! Теперь отправьте команду /generate, чтобы создать RSS-канал.")
 
 # обработчик команды /generate
-@dp.message_handler(commands=['generate'])
+@dp.message_handler(commands=["generate"])
 async def generate_rss(message: types.Message):
     # здесь необходимо добавить код для извлечения записей из веб-страницы
     feed = feedgen.feed.FeedGenerator()
-    feed.title('RSS-канал')
+    feed.title("RSS-канал")
+    feed.description("Моя RSS лента")
     # здесь можно получить сохраненную ссылку из базы данных или файла
-    link = 'https://example.com'
-    feed.link(href=link, rel='alternate')
+    link = "https://example.com"
+    feed.link(href=link, rel="alternate")
     # здесь необходимо добавить код для извлечения записей из веб-страницы
     entry = feed.add_entry()
-    entry.title(title)
-    entry.link(href=link, rel='alternate')
-    entry.description(text)
-    # после формирования RSS-канала отправляем его пользователю
+    entry.title("Заголовок статьи")
+    entry.link(href=link, rel="alternate")
+    entry.description(text("Описание статьи"))
+
+    # после формирования RSS-канала
     rss_feed = feed.rss_str(pretty=True)
     await message.answer(rss_feed)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
